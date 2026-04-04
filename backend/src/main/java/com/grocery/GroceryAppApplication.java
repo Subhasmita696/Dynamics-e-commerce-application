0f7a91ecee9d130a0694297279c042e140cbf5a7
Subhasmita696 <subhasmita696@example.com>
2026-04-04 21:41:04 +0200

    i build the application

---
+++ backend/src/main/java/com/grocery/GroceryAppApplication.java
@@ -0,0 +1,11 @@
+package com.grocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryAppApplication.class, args);
+    }
+}
